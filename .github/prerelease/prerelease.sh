39a92e9c0561b6c8721bd15e32328b25cb21adb0
FrancoAdN <francoadinapoli@gmail.com>
2022-06-14 17:37:02 -0300

    feat: CL-4 Publish pre-release workflow

---
+++ .github/prerelease/prerelease.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$(jq -r .version "./package.json")
+echo "Version $VERSION"
+git fetch --tags
+LAST_VERSION=$(git tag -l "v$VERSION-rc*" | sort -V | tail -n1)
+
+if [ -n "$LAST_VERSION" ]; then
+  npx semver "$LAST_VERSION" # Validate semver of last version
+  echo "Found last tagged version $LAST_VERSION"
+  TAG=$(npx semver -i prerelease "$LAST_VERSION")
+else
+  TAG="$VERSION-rc.0"
+fi
+
+echo "Tagging next pre-release v$TAG"
+git tag "v$TAG"
